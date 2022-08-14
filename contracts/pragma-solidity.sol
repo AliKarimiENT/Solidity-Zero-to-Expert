d420ae7dede0232aff3c23e85a8bdfbfd7f9deab
Remix Workspace <>
2022-08-14 11:45:54 +0430

    init pragma solidity

---
+++ contracts/pragma-solidity.sol
@@ -0,0 +1,5 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+// specifying the compiler version of solidity ranging and including 0.7.0 up to and not including 0.9.0
+
+// >= means greater than or equal to
+// < means less than
--- contracts/welcome-solidity.sol
+++ contracts/welcome-solidity.sol
@@ -1,5 +1,16 @@
-pragma solidity ^0.8.8;
+// assign the solidity version
+pragma solidity ^0.8.*;
 
 contract WelcomeSolidity {
-    
+    constructor() public {
+
+    }
+
+    // is adding two integers
+    function getResult() public view returns (uint){
+        uint a =1;
+        uint b = 2;
+        uint result = 5*(a+b);
+        return result;
+    }
 }
