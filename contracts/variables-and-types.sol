d3cb8a2539577fcc296e046ee204808f4c174bfe
Remix Workspace <>
2022-08-14 12:20:20 +0430

    init variables and types

---
+++ contracts/variables-and-types.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+// store data to variables
+// three main types of variables : Boolean , Integers and String
+
+contract LearnVariables{
+    uint chocolateBar = 10;
+    uint storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = "Error";
+}
